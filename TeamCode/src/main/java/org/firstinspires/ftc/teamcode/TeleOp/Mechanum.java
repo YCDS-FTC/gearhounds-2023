17bcc5f80dcd146e6c1907bb10213e2f1007ad3e
Linval <lgriffiths@ycds.org>
2023-12-21 15:31:21 -0500

    code from scrimmage

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mechanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Mechanum.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -45,7 +46,7 @@ public class Mechanum extends OpMode
      */
     @Override
     public void init_loop() {
-        telemetry.addLine("Hello Erik!");
+        telemetry.addLine("Hello Linval!");
         telemetry.update();
     }
 
@@ -88,32 +89,78 @@ public class Mechanum extends OpMode
             shift = 1;
         }
 
+        if (gamepad2.left_trigger != 0) {
+            robot.claw.setPower(gamepad2.left_trigger*.5);
+        } else if (gamepad2.right_trigger != 0) {
+            robot.claw.setPower(-gamepad2.right_trigger*.5);
+        } else robot.claw.setPower(0);
+
+
+        if (gamepad2.left_stick_y != 0) {
+            robot.lift.setPower(-gamepad2.left_stick_y);
+        } else if (gamepad2.left_stick_y != 0) {
+            robot.lift.setPower(gamepad2.left_stick_y);
+        } else robot.lift.setPower(0);
+
+        if (gamepad2.right_bumper)
+            robot.sticks.setPosition(.5);
+
+        if (gamepad2.left_bumper)
+            robot.sticks.setPosition(.45);
+
+
+
+//        if (gamepad1.x)
+//            robot.claw.setTargetPosition();
+//        robot.claw.setMode(DcMotor.);
+
+        /*if (gamepad1.right_bumper)
+            robot.intake.setPower(1);
+        else robot.intake.setPower(0);
+
+
+        if (gamepad1.left_bumper)
+            robot.intake.setPower(-1);
+        else robot.intake.setPower(0);
+*/
+
         telemetry.addData("", "Left Front %d   Right Front %d", robot.leftFront.getCurrentPosition(), robot.rightFront.getCurrentPosition());
 
         telemetry.addData("", "Left Back %d   Right Back %d", robot.leftBack.getCurrentPosition(), robot.rightBack.getCurrentPosition());
 
         telemetry.addData("", "Angle  %f", robot.getAngle());
 
+        telemetry.addData("", "lift %d", robot.lift.getCurrentPosition());
+
+        telemetry.addData("", "claw %d", robot.claw.getCurrentPosition());
+
         telemetry.update();
 
-        // Driver centric mechanum
-        Orientation angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
-        double angle = -angles.firstAngle;
-        double r = Math.hypot(-gamepad1.left_stick_x, gamepad1.left_stick_y);
-        double robotAngle = Math.atan2(-gamepad1.left_stick_y, -gamepad1.left_stick_x) + Math.PI / 4;
-        robotAngle = robotAngle - Math.toRadians(angle);
-        double rightX = gamepad1.right_stick_x;
-        final double lf = -r * Math.cos(robotAngle) + rightX;
-        final double lb = r * Math.sin(robotAngle) + rightX;
-        final double rf = -r * Math.cos(robotAngle) - rightX;
-        final double rb = r * Math.sin(robotAngle) - rightX;
-        //final double i = r * Math.sin(robotAngle) + rightX;
+        //code taken from https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
+        double facing = robot.imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+        if (gamepad1.options) {
+            robot.imu.resetYaw();
+        }
 
-        robot.leftFront.setPower(lf * shift);
-        robot.leftBack.setPower(lb * shift);
-        robot.rightFront.setPower(rf * shift);
-        robot.rightBack.setPower(rb * shift);
-        //robot.intake.equals( * shift);
+        double rotX = x * Math.cos(-facing) - y * Math.sin(-facing);
+        rotX = rotX * 1.1;
+        double rotY = x * Math.sin(-facing) + y * Math.cos(-facing);
+
+        double d = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+
+        double lf = (rotY + rotX + rx) / d;
+        double lb = (rotY - rotX + rx) / d;
+        double rf = (rotY - rotX - rx) / d;
+        double rb = (rotY + rotX - rx) / d;
+
+        //set rightFront negative so it goes same direction as other heels
+        robot.leftFront.setVelocity(2000 * lf * shift);
+        robot.leftBack.setVelocity(2000 * lb * shift);
+        robot.rightFront.setVelocity(2000 * rf * shift);
+        robot.rightBack.setVelocity(2000 * rb * shift);
 
 
     }
@@ -129,7 +176,9 @@ public class Mechanum extends OpMode
         robot.rightFront.setPower(0);
         robot.leftBack.setPower(0);
         robot.rightBack.setPower(0);
-        //robot.intake.equals(0);
+        robot.lift.setPower(0);
+        robot.claw.setPower(0);
+        robot.sticks.setPosition(0);
     }
 
     // fuction used to make sure that the value inputted to the
